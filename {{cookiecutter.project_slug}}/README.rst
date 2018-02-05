a7a4974913068d772808eb281bef2a09b13aff55
syonekura <sebastian.yonekura@cencosud.cl>
2018-02-05 09:14:13 -0300

    first commit

---
+++ {{cookiecutter.project_slug}}/README.rst
@@ -0,0 +1,19 @@
+{% for _ in cookiecutter.project_name %}={% endfor %}
+{{ cookiecutter.project_name }}
+{% for _ in cookiecutter.project_name %}={% endfor %}
+
+{{ cookiecutter.project_short_description }}
+
+Features
+--------
+
+* TODO
+
+Credits
+---------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
+
---
+++ {{cookiecutter.project_slug}}/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    {%- if cookiecutter.command_line_interface|lower == 'click' %}
+    'Click>=6.7',
+    {%- endif %}
+    # TODO: put package requirements here
+]
+
+setup_requirements = [
+{%- if cookiecutter.use_pytest == 'y' %}
+    'pytest-runner',
+{%- endif %}
+    # TODO: put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+{%- if cookiecutter.use_pytest == 'y' %}
+    'pytest',
+{%- endif %}
+    # TODO: put package test requirements here
+]
+
+{%- set license_classifiers = {
+    'MIT license': 'License :: OSI Approved :: MIT License',
+    'BSD license': 'License :: OSI Approved :: BSD License',
+    'ISC license': 'License :: OSI Approved :: ISC License (ISCL)',
+    'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
+    'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+} %}
+
+setup(
+    name='{{ cookiecutter.project_slug }}',
+    version='{{ cookiecutter.version }}',
+    description="{{ cookiecutter.project_short_description }}",
+    long_description=readme + '\n\n' + history,
+    author="{{ cookiecutter.full_name.replace('\"', '\\\"') }}",
+    author_email='{{ cookiecutter.email }}',
+    packages=find_packages(include=['{{ cookiecutter.project_slug }}']),
+    {%- if 'no' not in cookiecutter.command_line_interface|lower %}
+    entry_points={
+        'console_scripts': [
+            '{{ cookiecutter.project_slug }}={{ cookiecutter.project_slug }}.cli:main'
+        ]
+    },
+    {%- endif %}
+    include_package_data=True,
+    install_requires=requirements,
+    zip_safe=False,
+    keywords='{{ cookiecutter.project_slug }}',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6'
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
